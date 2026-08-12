57d2ff20dceab661d4838d133e11c39389b559b0
fabioseixas <dev.fabioseixas@gmail.com>
2026-08-12 11:40:56 -0300

    add assets

---
+++ assets/experiment.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="experiment" tilewidth="64" tileheight="64" tilecount="126" columns="9">
+ <image source="experiment.aseprite" width="633" height="919"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.125" y="54.125">
+    <polygon points="0,0 18.5,9.375 48.75,-6.25 30.25,-15.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="1.25" y="47.5">
+    <polygon points="0,0 31.125,-15.875 48.875,-6.625 18.25,8.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.625" y="46.75">
+    <polygon points="0,0 8.875,4.5 39.625,-10.875 30.875,-14.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23.25" y="58.625">
+    <polygon points="0,0 9.25,4.75 40.25,-11.125 30.625,-16.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="1.125" y="46.875">
+    <polygon points="0,0 30.125,15.375 61.25,-0.125 30.5,-17.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup draworder="index" id="2">
+   <object id="3" template="box_collision.tx" x="1.5" y="48.125"/>
+  </objectgroup>
+ </tile>
+</tileset>
